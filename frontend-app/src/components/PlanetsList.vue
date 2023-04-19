5d2db21c458e43beebb6b99cad487a5fee22ef3e
¨kemken077¨ <kemken077@gmail.com>
2023-04-19 13:15:57 -0300

    PlanetsList component

---
+++ frontend-app/src/components/PlanetsList.vue
@@ -0,0 +1,37 @@
+<script>
+import { getPlanets } from '../services/PlanetService';
+
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    // Planet,
+  },
+  data() {
+    return {
+      planets: [],
+      title: 'Planets:',
+    }
+  },
+  mounted() {
+    console.log('planets mounted');
+    this.planets = getPlanets();
+    this.planets.then((res) => {
+      this.planets = res;
+    });
+  },
+  methods: {
+    // 
+  }
+}
+
+
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <li v-for="(planet, index) in planets" :key="index">
+    {{ planet.name }} - {{ index }}
+  </li>
+</template>
+
+<style scoped></style>
