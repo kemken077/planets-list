6efaa955f97ffc40081cf3112f6891a89d9517b2
¨kemken077¨ <kemken077@gmail.com>
2023-04-21 19:10:35 -0300

    More button component

---
+++ frontend-app/src/components/MoreButton.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: {
+    text: String,
+    ctaURL: String,
+  },
+  data() {
+    return {  
+    };
+  },
+  methods: {
+    handleMoreClick(e) {
+      console.log('Click [more+]');
+      console.log(e.target);
+    }
+  },
+}
+</script>
+
+<template>
+  <li class="more" @click="handleMoreClick">
+    {{ text }}
+  </li>
+</template>
+
+<style scoped>
+.more {
+  color: rgba(235, 235, 235, 0.64);
+  background-color: #181818;
+}
+.more:hover {
+  color: pink;
+  border: 1px solid pink;
+  background-color: transparent;
+  padding: 20px 10px;
+}
+</style>
