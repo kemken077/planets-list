f64a56679a9fb6deaf405d9eaa762010215b8781
¨kemken077¨ <kemken077@gmail.com>
2023-04-21 19:09:57 -0300

    Work on more button

--- frontend-app/src/components/PlanetsGrid.vue
+++ frontend-app/src/components/PlanetsGrid.vue
@@ -1,8 +1,10 @@
 <script>
 import PlanetItem from '../components/PlanetItem.vue';
+import MoreButton from '../components/MoreButton.vue';
 export default {
   components: {
     PlanetItem,
+    MoreButton,
   },
   props: {
     items: Array,
@@ -18,19 +20,19 @@ export default {
     <li v-for="(item, index) in items" :key="index">
       <PlanetItem :name="item.name" />
     </li>
+    <MoreButton :text="'+more'" />
   </ul>
 </template>
 
 <style scoped>
-h1,
-li {
-  color: pink;
-}
 ul {
   list-style-type: none;
 }
+li {
+  color: pink;
+}
 li:hover {
-  background-color: pink;;
+  background-color: pink;
   color: #181818;
   cursor: pointer;
 }
